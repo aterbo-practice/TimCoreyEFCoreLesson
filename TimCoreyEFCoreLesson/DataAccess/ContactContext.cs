9731058a502c5787451a86052125b8fb942076e3
aterbo <andy.terbovich@gmail.com>
2021-09-28 00:58:08 -0400

    Add Context : DbContext file and initial setup

---
+++ TimCoreyEFCoreLesson/DataAccess/ContactContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace TimCoreyEFCoreLesson
+{
+    public class ContactContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            options.UseSqlServer(config.GetConnectionString("Default"));
+        }
+
+    }
+}
--- TimCoreyEFCoreLesson/Program.cs
+++ TimCoreyEFCoreLesson/Program.cs
@@ -7,6 +7,30 @@ namespace TimCoreyEFCoreLesson
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.ReadLine();
         }
     }
 }
+
+/************************************************************
+NOTES: Tim thinks EF pushes Data Access higher into the UI Layer, when it should be separate
+(probably in a class library or something). This example has Data Access folder because it's
+in line with how EF works, but he prefers to put it all in a separate library
+
+Code-First EF creation
+Steps:
+1. Create basic Models (Email, Phone, Contact)
+2. Add NuGet packages
+3. Create appsettings.json with trusted SQL Server connection string from 
+    connectionstrings.com
+4. Create Data Access layer by setting up Context
+    a. ContactContext : DbContext
+    b. Standard console app builder/config/appsettings setup    
+    c. Override OnConfiguring to set UseSqlServer()
+    
+
+
+
+
+
+*************************************************************/
