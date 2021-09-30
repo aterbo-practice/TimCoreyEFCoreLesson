c44c5eeb2fe09cf8543b6fe15426c9db18b7e12c
aterbo <andy.terbovich@gmail.com>
2021-09-30 01:42:52 -0400

    Add new migration and Update Database

--- TimCoreyEFCoreLesson/Migrations/20210930054159_ImprovedDb.Designer.cs
+++ TimCoreyEFCoreLesson/Migrations/20210930054159_ImprovedDb.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TimCoreyEFCoreLesson;
 
 namespace TimCoreyEFCoreLesson.Migrations
 {
     [DbContext(typeof(ContactContext))]
-    partial class ContactContextModelSnapshot : ModelSnapshot
+    [Migration("20210930054159_ImprovedDb")]
+    partial class ImprovedDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
